132623495d79c7d3bf91e0acb229fd10f451bc28
guyang66 <13588295865@163.com>
2021-07-14 11:38:17 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDom from 'react-dom';
 import React from 'react';
 import './index.styl';
+import { Route } from 'react-router-dom';
 import Frame from './views/components/frame';
 
 ReactDom.render(
